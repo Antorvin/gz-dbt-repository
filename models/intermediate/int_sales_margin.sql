9ffb1404913fff4e978831c8fb7d1421bcdb793a
corvino.antonio89@gmail.com <corvino.antonio89@gmail.com>
2023-10-31 15:33:54 +0000

    sales margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,5 +1,6 @@
 select 
-cast(purchse_price as INT64)
+cast(purchase_price as float64)*quantity as purchase_cost
+,round(revenue-(cast(purchase_price as float64)),2) as margin
 from {{ ref("stg_gz_raw_data__raw_gz_product_copy")}}
 join {{ ref("stg_gz_raw_data__raw_gz_sales_copy")}}
 on product_id = products_id
