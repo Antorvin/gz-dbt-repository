33c6329a83e7b3323b91b461fe2db376ea5f354c
corvino.antonio89@gmail.com <corvino.antonio89@gmail.com>
2023-11-01 14:11:34 +0000

    creation of finance analisys

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,5 +1,6 @@
 select 
-cast(purchase_price as float64)*quantity as purchase_cost
+*
+,cast(purchase_price as float64)*quantity as purchase_cost
 ,round(revenue-(cast(purchase_price as float64)),2) as margin
 from {{ ref("stg_gz_raw_data__raw_gz_product_copy")}}
 join {{ ref("stg_gz_raw_data__raw_gz_sales_copy")}}
